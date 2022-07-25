7ac7aaf237129fa9a00b03cbd9d888491c9a2c77
amsanjayx <86143987+amsanjayx@users.noreply.github.com>
2022-07-25 21:42:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,79 +1,105 @@
 /* Fill your code*/
-function formValidate(){
+function formValidate()
+{
+    let name_regex = /^.{8,15}$/;
+    let email_regex = /\S+@\S+\.|\_\S+/;
+    let password_regex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()]).{8,20}$/;
+    let phone_regex = /^[6-9]\d{9}$/;
 
-    var name = document.forms["RegForm"]["Name"];
-    var address = document.forms["RegForm"]["Address"];
-    var email = document.forms["RegForm"]["EMail"];
-    var password = document.forms["RegForm"]["Password"];
-    var cpassword = document.forms["RegForm"]["cPassword"];
-    var number = document.forms["RegForm"]["phone"];
-    
-    var errname = document.getElementById("name");
-    var erradd = document.getElementById("address");
-    var erremail = document.getElementById("email");
-    var errpass = document.getElementById("pwd");
-    var errcnf = document.getElementById("cpwd");
-    var errnum = document.getElementById("phone");
-    
-    //check and validate the form elements
-    
-    if(name.value.length < 8 ){
-        errname.innerText = 'Name must be more than 4 characters';
-        name.focus();
-        return false;
-     }
-    else{
-        errname.innerText = '';
-     }
-    
-    if(address.value == ''){
-        erradd.innerText = 'Address cannot be empty';
-        address.focus();
-        return false;
-     }
-    else{
-        erradd.innerText = '';
+    let name = document.getElementById("NAme");
+    let add = document.getElementById("ADDress");
+    let email = document.getElementById("EMail");
+    let password = document.getElementById("PASSword");
+    let confirm_password = document.getElementById("CPAssword");
+    let phone = document.getElementById("PHone");
+
+    let error_name = document.getElementById("name");
+    let error_add = document.getElementById("address");
+    let error_email = document.getElementById("email");
+    let error_password = document.getElementById("pwd");
+    let error_cpassword = document.getElementById("cpwd");
+    let error_phone = document.getElementById("phone");
+
+    let count = 0;
+
+    if(name_regex.test(name.value))
+    {
+        error_name.textContent="";
+        count++;
     }
-    
-    if(email.value == ''){
-        erremail.innerText = 'Email cannot be empty';
-        email.focus();
-        return false;
-     }
-    else if(/^[a-zA-Z0-9._]{3,}@[A-Za-z]{3,}[.]{1}[a-zA-Z.]{2,6}$/.test(email.value)){
-        erremail.innerText = '';
-     }
-    else{
-        erremail.innerText = 'Email is invalid';
-        email.focus();
-        return false;
-     }
-    
-      if(/(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#\$%\^&\*])/.test(password.value)){
-         errpass.innerText = '';
-     }
-     else{
-        errpass.innerText = 'Password is invalid';
-        password.focus();
-        return false;
-     }
-    
-     if(cpassword.value == password.value){
-        errcnf.innerText = '';
-    }
-    else{
-       errcnf.innerText = 'Password donnot match';
-       cpassword.focus();
-       return false;
+    else
+    {
+        error_name.textContent="Atleast 8 to 15 Character Required";
     }
 
-     if(number.value.length < 10 || number.value.length > 10){
-         errnum.innerText = 'Invalid number';
-         errnum.focus();
-         return false;
-     }
-     else{
-         errnum.innerText = ''
-     }
-     return true;
+    if(add.value=="")
+    {
+        error_add.textContent="Address cannot be empty. Kindly fill ";
+    }
+    else
+    {
+        error_add.textContent="";
+        count++;
+    }
+
+    if(email.value=="")
+    {
+        error_email.textContent="Email cannot be empty. Kindly fill ";
+    }
+    else if(email_regex .test(email.value))
+    {
+        error_email.textContent="";
+        count++;
+    }
+    else
+    {
+        error_email.textContent="Enter a vaild Email";
+    }
+
+    if(password.value=="")
+    {
+        error_password.textContent="Kindly fill the Password";
+    }
+    else if(password_regex.test(password.value))
+    {
+        error_password.textContent="";
+        count++;
+    }
+    else
+    {
+        error_password.textContent="Must contain 1 uppercase, 1 lowercase, 1 number & any Symbols";
+    }
+
+    if(confirm_password.value=="")
+    {
+        error_cpassword.textContent="Kindly confirm your password";
+    }
+    else if(confirm_password.value==password.value)
+    {
+        error_cpassword.textContent="";
+        count++;
+    }
+    else
+    {
+        error_confirm_password.textContent="Password and confirm password must be same";
+    }
+
+    if(phone.value=="")
+    {
+        error_phone.textContent="Kindly fill the phone number";
+    }
+    else if(phone_regex.test(phone.value))
+    {
+        error_phone.textContent="";
+        count++;
+    }
+    else
+    {
+        error_phone.textContent="Enter a valid 10 digit number"; 
+    }
+
+    if (count === 6)
+    {
+        alert("Regex Validation Success");
+    }
 }
